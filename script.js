368d2f2943f5a023c6c7913abc34e146fc9012fe
IwonaMaurer <plastimex2@wp.pl>
2017-12-09 21:18:37 +0100

    zadanie

--- script.js
+++ script.js
@@ -9,7 +9,15 @@ React.createElement('div', {},
     React.createElement('li', {},
       React.createElement('h2', {}, 'Król Lew'),
       React.createElement('p', {}, 'Film opowiadający historię króla sawanny')
-  )
+  ),
+  React.createElement('li', {},
+  React.createElement('h2', {}, 'Gwiezdne wojny'),
+  React.createElement('p', {}, 'opis')
+),
+React.createElement('li', {},
+React.createElement('h2', {}, 'Milczenie owiec'),
+React.createElement('p', {}, 'opis')
+)
 );
 
 var movies = [
@@ -22,13 +30,23 @@ var movies = [
     id: 2,
     title: 'Król Lew',
     desc: 'Film o królu sawanny'
+  },
+  {
+    id: 3,
+    title: 'Gwiezdne wojny',
+    desc: 'opis'
+  },
+  {
+    id: 4,
+    title: 'Milczenie owiec',
+    desc: 'opis'                        
   }
 ];
-
+                         
 var moviesElements = movies.map(function(movie) {
   return React.createElement('li', {key: movie.id},
       React.createElement('h2', {}, movie.title),
-      React.createElement('p', {}, movie.desc)
+      React.createElement('p', {}, movie.desc),
     );
 });
 
