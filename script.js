10963cbe0266f09c88079f09e156a7c76b725251
IwonaMaurer <plastimex2@wp.pl>
2017-12-09 20:48:49 +0100

    zadanie

--- script.js
+++ script.js
@@ -1,48 +1,37 @@
-
-
 var element =
 React.createElement('div', {},
   React.createElement('h1', {}, 'Lista filmów'),
-  React.createElement('ul', {}, moviesElements),
-  React.createElement('img', {})
+  React.createElement('ul', {},  moviesElements),
+    React.createElement('li', {},
+      React.createElement('h2', {}, 'Harry Potter'),
+      React.createElement('p', {}, 'Film o czarodzieju')
+    ),
+    React.createElement('li', {},
+      React.createElement('h2', {}, 'Król Lew'),
+      React.createElement('p', {}, 'Film opowiadający historię króla sawanny')
+  )
 );
 
-
 var movies = [
-    {
-      id: 1,
-      title: 'Harry Potter',
-      desc: 'film o czarodzieju',
-      src: 'http://imgur.com/n8OYCzR.png'
-    },
-    {
-      id: 2,
-      title: 'Król Lew',
-      desc: 'Film o królu sawanny',
-      src: 'http://imgur.com/n8OYCzR.png'
-    },
-    {
-      id: 3,
-      title: 'Gwiezdne wojny',
-      desc: 'Ostatni Jedi',
-      src: 'http://imgur.com/n8OYCzR.png'
-    },
-    {
-      id: 4,
-      title: 'Morderstwo w Orient Expressie',
-      desc: 'film kryminalny',
-      src: 'http://imgur.com/n8OYCzR.png'
-    }
-  ];
+  {
+    id: 1,
+    title: 'Harry Potter',
+    desc: 'film o czarodzieju'
+  },
+  {
+    id: 2,
+    title: 'Król Lew',
+    desc: 'Film o królu sawanny'
+  }
+];
 
+var moviesElements = movies.map(function(movie) {
+  return React.createElement('li', {key: movie.id},
+      React.createElement('h2', {}, movie.title),
+      React.createElement('p', {}, movie.desc)
+    );
+});
 
-  var moviesElements = movies.map(function(movie) {
-    return React.createElement('li', {key: movie.id},
-        React.createElement('h2', {}, movie.title),
-        React.createElement('p', {}, movie.desc),
-        React.createElement('img', {}, movie.src)
-      );
-  });
   ReactDOM.render(element, document.getElementById('app'));
 
  
