56eaa71febece7190f0521292ae703ba2b9d94de
IwonaMaurer <plastimex2@wp.pl>
2017-12-12 08:28:13 +0100

    zadanie

--- script.js
+++ script.js
@@ -1,55 +1,48 @@
-var element =
-React.createElement('div', {},
-  React.createElement('h1', {}, 'Lista filmów'),
-  React.createElement('ul', {},  moviesElements),
-    React.createElement('li', {},
-      React.createElement('h2', {}, 'Harry Potter'),
-      React.createElement('p', {}, 'Film o czarodzieju')
-    ),
-    React.createElement('li', {},
-      React.createElement('h2', {}, 'Król Lew'),
-      React.createElement('p', {}, 'Film opowiadający historię króla sawanny')
-  ),
-  React.createElement('li', {},
-  React.createElement('h2', {}, 'Gwiezdne wojny'),
-  React.createElement('p', {}, 'opis')
-),
-React.createElement('li', {},
-React.createElement('h2', {}, 'Milczenie owiec'),
-React.createElement('p', {}, 'opis')
-)
-);
+
 
 var movies = [
   {
     id: 1,
     title: 'Harry Potter',
-    desc: 'film o czarodzieju'
+    desc: 'film o czarodzieju',
+    src: 'http://imgur.com/n8OYCzR.png'
   },
   {
     id: 2,
     title: 'Król Lew',
-    desc: 'Film o królu sawanny'
+    desc: 'Film o królu sawanny',
+    src: 'http://imgur.com/n8OYCzR.png'
   },
   {
     id: 3,
     title: 'Gwiezdne wojny',
-    desc: 'opis'
+    desc: 'opis',
+    src: 'http://imgur.com/n8OYCzR.png'
   },
   {
     id: 4,
     title: 'Milczenie owiec',
-    desc: 'opis'                        
+    desc: 'opis',
+    src: 'http://imgur.com/n8OYCzR.png'                     
   }
 ];
-                         
+
+
+
 var moviesElements = movies.map(function(movie) {
   return React.createElement('li', {key: movie.id},
       React.createElement('h2', {}, movie.title),
       React.createElement('p', {}, movie.desc),
+  React.createElement('img', {src: movie.src})
     );
 });
-
+var element =
+React.createElement('div', {},
+  React.createElement('h1', {}, 'Lista filmów'),
+  React.createElement('ul', {}, moviesElements)
+);
   ReactDOM.render(element, document.getElementById('app'));
+
+ 
 
  
